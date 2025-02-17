189d7e9f6c6c38cda1e87243308c9f6dc9ac81ff
Anshu Raj <158818764+Anshu-ParadoxInnovator@users.noreply.github.com>
2025-02-17 15:23:56 +0530

    updated contact form on contact us section with backend

--- src/Components/Location/location.jsx
+++ src/Components/Location/location.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./location.module.css"; // Make sure to create the corresponding CSS file
 import { FaMapMarkerAlt } from "react-icons/fa"; // Using map marker icon from react-icons
 
+import ContactForm from '../../Components/ContactForm/ContactForm'
+
 const Location = () => {
   return (
     <div className={styles.locationContainer}>
@@ -12,14 +14,14 @@ const Location = () => {
           We're here to help! Find our location below.
         </p>
       </header>
-
+      <ContactForm />
       {/* Title and Description Section */}
       <div className={styles.locationInfo}>
         <h2 className={styles.locationTitle}>
           <FaMapMarkerAlt className={styles.icon} /> Location
         </h2>
         <p className={styles.locationDescription}>
-          Located at Room No. 602, Heritage Institute of Technology, Kolkata.
+          Located at 8/8 Kalitala Link Road Kalikapur, West Bengal - 700078.
         </p>
         <p className={styles.additionalInfo}>
           Visit us to explore our innovative solutions and collaborations.
@@ -30,7 +32,7 @@ const Location = () => {
       <div className={styles.mapContainer}>
         <iframe
           className={styles.map}
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11672.24450288334!2d88.4187955!3d22.5172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0273f58b9feec5%3A0x30f8067b73c45d8!2sHeritage+Institute+of+Technology%2C+Kolkata!5e0!3m2!1sen!2sin!4v1675492033344!5m2!1sen!2sin"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d933.6595266050366!2d88.39703903228674!3d22.507230809085783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0271572ac20f19%3A0xd006bf2100e5b881!2s8%2F8%2C%20North%20Purbachal%2C%20Haltu%2C%20Kolkata%2C%20West%20Bengal%20700078!5e0!3m2!1sen!2sin!4v1739785775502!5m2!1sen!2sin"
           width="100%"
           height="450"
           style={{ border: 0 }}
--- src/Pages/Contact/contact.jsx
+++ src/Pages/Contact/contact.jsx
@@ -8,7 +8,6 @@ const contact = () => {
   return (
     <div>
       <Navbar />
-     
       <Location />
       <Footer />
     </div>
