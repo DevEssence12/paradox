6f3e9c11b9cbc6f05676677a24d7e13021bf5c7d
Anshu Raj <158818764+Anshu-ParadoxInnovator@users.noreply.github.com>
2025-02-17 16:12:24 +0530

    updated contact form on contact us section with backend

--- src/Components/Social/Social.jsx
+++ src/Components/Social/Social.jsx
@@ -15,10 +15,14 @@ const SocialIcons = () => {
     <div className={styles.iconContainer}>
       <div className={styles.socialIcons}>
         <button className={styles.whatsappIcon} onClick={handleWhatsappClick}>
-          <FaWhatsapp size={24} />
+          {/* <FaWhatsapp size={24} /> */}
+
+          <img src="https://i.postimg.cc/sDy17fWj/whatsapp-logo-7-removebg-preview.png" width="50px" class="wpftbtn" alt="Whatsapp"></img>
         </button>
         <button className={styles.phoneIcon} onClick={handlePhoneClick}>
-          <FaPhone size={24} />
+          {/* <FaPhone size={24} /> */}
+          <img src="https://i.ibb.co/FqqXrf95/phone-call.png" width="50px" class="wpftbtn" alt="Whatsapp"></img>
+        
         </button>
       </div>
     </div>
